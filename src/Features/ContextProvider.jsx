165594ce09b502f6234471a955a79cb343228d06
PabloFC <pablofc_68@hotmail.com>
2024-09-11 13:55:23 +0200

    context provider added

---
+++ src/Features/ContextProvider.jsx
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from "react";
+import CartReducer from "./CartReducer";
+
+const CartContext = createContext();
+
+const ContextProvider = ({ children }) => {
+  const [cart, dispatch] = useReducer(CartReducer, []);
+
+  return (
+    <CartContext.Provider value={{ cart, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default ContextProvider;
