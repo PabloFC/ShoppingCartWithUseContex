348bcb5b7f72ac947a68d4ecda52c86e6d79159c
PabloFC <pablofc_68@hotmail.com>
2024-09-11 13:15:04 +0200

    adding Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="py-3 bg-secondary d-flex justify-content-center">
+      <p className="fw-bolder fs-4 text-white">&copy; PabloFC</p>
+    </div>
+  );
+};
+
+export default Footer;
