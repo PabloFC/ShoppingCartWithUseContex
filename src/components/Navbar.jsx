92adfa09f53df43097f2a426b106827a7cf7f68a
PabloFC <pablofc_68@hotmail.com>
2024-09-16 14:10:25 +0200

    fixing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,22 @@ import { CartContext } from "../Features/ContextProvider";
 
 const Navbar = () => {
   const { cart } = useContext(CartContext);
+
   return (
     <div className="d-flex justify-content-between bg-secondary py-3 px-5 text-white">
       <Link to="/" className="navbar-brand fs-4 fw-bolder">
         Shop
       </Link>
-      <Link to="/cart" className="navbar-link fs-5 text-white">
-        <BsCart />
-        {cart.length}
-      </Link>
+      {cart.length > 0 ? (
+        <Link to="/cart" className="navbar-link fs-5 text-white">
+          <BsCart />
+          <span>{cart.length}</span>
+        </Link>
+      ) : (
+        <div className="navbar-link fs-5 text-white">
+          <BsCart />
+        </div>
+      )}
     </div>
   );
 };
